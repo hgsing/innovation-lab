3fd3cf5b2833381f0a9eca1840f57a3502ba3a05
HS <hsinglet@gmu.edu>
2023-06-15 17:39:01 -0400

    added basic test

--- website/ecosystem/tests.py
+++ website/ecosystem/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from unittest import TestCase
+from . import information_provider, state
 
 # Create your tests here.
+class DevicesTestCase(TestCase):
+    cfg = None
+
+    def setUp(self) -> None:
+        self.cfg = state.config
+
+    def test_devices_loaded(self):
+        self.assertGreater(len(state.devices), 0)
+
+    
